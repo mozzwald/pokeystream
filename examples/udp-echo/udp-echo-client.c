6efd115848bc0f478aaba382fc9ce418f79de82b
mozzwald <mozzwald@gmail.com>
2026-01-09 21:47:58 -0600

    Update example: Live character streaming

--- examples/udp-echo/udp-echo-client.c
+++ examples/udp-echo/udp-echo-client.c
@@ -7,97 +7,133 @@
 
 #include "pokeystream.h"
 
-static uint16_t append_eol(char* dst, uint16_t len, uint16_t cap)
-{
-    if (len < cap) dst[len++] = (char)0x9B;
-    if (len < cap) dst[len] = '\0';
-    return len;
-}
-
-static uint16_t to_atascii_line(const char* line, char* out, uint16_t cap)
-{
-    uint16_t i = 0;
-    while (line[i] && i + 1 < cap) {
-        if (line[i] == '\r') {
-            ++i;
-            continue;
-        }
-        if (line[i] == '\n') break;
-        out[i] = line[i];
-        ++i;
-    }
-    return append_eol(out, i, cap);
-}
-
-static void send_line(const char* line)
-{
-    char buf[128];
-    uint16_t len = to_atascii_line(line, buf, (uint16_t)sizeof(buf) - 1);
-    ps_send(buf, len);
-    ps_flush_tx();
-}
-
-static void print_prompt(const char* input, unsigned len)
+static void render_input_line(const char* input,
+                              unsigned len,
+                              unsigned char y,
+                              unsigned char width)
 {
     unsigned i = 0;
-    printf("\n> ");
-    while (i < len) {
-        cputc(input[i]);
-        ++i;
+    if (width < 2) return;
+    cputcxy(0, y, '>');
+    cputcxy(1, y, ' ');
+    for (i = 0; i + 2 < width; ++i) {
+        char ch = (i < len) ? input[i] : ' ';
+        cputcxy((unsigned char)(2 + i), y, ch);
+    }
+    if (y + 1 < 24) {
+        for (i = 0; i < width; ++i) {
+            char ch = (i + (width - 2) < len) ? input[i + (width - 2)] : ' ';
+            cputcxy((unsigned char)i, (unsigned char)(y + 1), ch);
+        }
+    }
+}
+
+static void scroll_region(unsigned char top, unsigned char bottom, unsigned char width)
+{
+    unsigned char y = top;
+    if (bottom <= top) return;
+    while (y < bottom) {
+        unsigned char x = 0;
+        while (x < width) {
+            gotoxy(x, (unsigned char)(y + 1));
+            {
+                char ch = cpeekc();
+                gotoxy(x, y);
+                cputc(ch);
+            }
+            ++x;
+        }
+        ++y;
+    }
+    cclearxy(0, bottom, width);
+}
+
+static void remote_putc(char c,
+                        unsigned char* cur_x,
+                        unsigned char* cur_y,
+                        unsigned char top,
+                        unsigned char bottom,
+                        unsigned char width)
+{
+    if (c == (char)0x9B) {
+        *cur_x = 0;
+        if (*cur_y >= bottom) {
+            scroll_region(top, bottom, width);
+        } else {
+            ++(*cur_y);
+        }
+        return;
+    }
+
+    gotoxy(*cur_x, *cur_y);
+    cputc(c);
+    if (++(*cur_x) >= width) {
+        *cur_x = 0;
+        if (*cur_y >= bottom) {
+            scroll_region(top, bottom, width);
+        } else {
+            ++(*cur_y);
+        }
     }
 }
 
 int main(void)
 {
     char line[128];
-    char rx_line[128];
-    unsigned rx_len = 0;
     unsigned line_len = 0;
+    unsigned char screen_w = 0;
+    unsigned char screen_h = 0;
+    unsigned char input_y = 0;
+    unsigned char rx_x = 0;
+    unsigned char rx_y = 0;
+    unsigned char rx_top = 0;
 
     clrscr();
-    cprintf("POKEYStream UDP Echo Demo Atari Client\r\n");
-    cprintf("Type a line and press ENTER.\r\n\r\n");
+    gotoxy(0, 0);
+    cputs("POKEYStream UDP Echo Live Client");
 
     ps_init();
+
+    screensize(&screen_w, &screen_h);
+    if (screen_w == 0) screen_w = 40;
+    if (screen_h < 2) screen_h = 24;
+    input_y = 1;
+    rx_top = 3;
+    rx_x = 0;
+    rx_y = rx_top;
+    gotoxy(0, 2);
+    cclearxy(0, 2, screen_w);
+    cputs("-------------------------------");
 
     for (;;) {
         uint8_t b;
 
         line_len = 0;
         line[0] = '\0';
-        print_prompt(line, line_len);
+        render_input_line(line, line_len, input_y, screen_w);
 
         for (;;) {
             while (ps_read_byte(&b)) {
-                if (b == 0x9B) {
-                    rx_line[rx_len] = '\0';
-                    printf("\n%s\n", rx_line);
-                    rx_len = 0;
-                    print_prompt(line, line_len);
-                } else if (rx_len + 1 < sizeof(rx_line)) {
-                    rx_line[rx_len++] = (char)b;
-                }
+                remote_putc((char)b, &rx_x, &rx_y, rx_top,
+                            (unsigned char)(screen_h - 1), screen_w);
+                render_input_line(line, line_len, input_y, screen_w);
             }
 
             if (kbhit()) {
                 char ch = cgetc();
                 if (ch == '\r' || ch == '\n') {
-                    cputc('\n');
-                    line[line_len] = '\0';
-                    send_line(line);
-                    rx_len = 0;
+                    ps_send_byte((uint8_t)0x9B);
+                    ps_flush_tx();
+                    line_len = 0;
+                    line[0] = '\0';
+                    render_input_line(line, line_len, input_y, screen_w);
                     break;
-                }
-                if ((ch == 8 || ch == 127) && line_len > 0) {
-                    --line_len;
-                    gotox(wherex() - 1);
-                    cputc(' ');
-                    gotox(wherex() - 1);
-                    continue;
                 }
                 if (line_len + 1 < sizeof(line)) {
                     line[line_len++] = ch;
-                    cputc(ch);
+                    ps_send_byte((uint8_t)ch);
+                    ps_flush_tx();
+                    render_input_line(line, line_len, input_y, screen_w);
                 }
             }
         }
--- examples/udp-echo/udp-echo-server.c
+++ examples/udp-echo/udp-echo-server.c
@@ -9,13 +9,14 @@
  */
 
 #define _POSIX_C_SOURCE 200809L
-#include <arpa/inet.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
+#include <sys/ioctl.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -33,16 +34,98 @@ static long parse_port(const char* s)
     return p;
 }
 
-static size_t build_line(char* out, size_t cap, const char* payload, size_t len)
+static struct termios orig_term;
+static int term_rows = 24;
+static int term_cols = 80;
+
+static void restore_term(void)
 {
-    size_t i = 0;
-    for (size_t j = 0; j < len && i + 1 < cap; ++j) {
-        if ((unsigned char)payload[j] == 0x9B) break;
-        out[i++] = payload[j];
+    tcsetattr(STDIN_FILENO, TCSANOW, &orig_term);
+}
+
+static void set_raw_term(void)
+{
+    struct termios t;
+    tcgetattr(STDIN_FILENO, &orig_term);
+    t = orig_term;
+    t.c_lflag &= (unsigned)(~(ICANON | ECHO));
+    t.c_cc[VMIN] = 0;
+    t.c_cc[VTIME] = 0;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+    atexit(restore_term);
+}
+
+static void move_cursor(int row, int col)
+{
+    printf("\033[%d;%dH", row, col);
+}
+
+static void show_prompt(const char* buf, size_t len, int input_row)
+{
+    move_cursor(input_row, 1);
+    printf("\033[K> ");
+    if (len > 0) fwrite(buf, 1, len, stdout);
+    fflush(stdout);
+}
+
+static void clear_rx_region(int rx_top, int rx_bottom)
+{
+    for (int r = rx_top; r <= rx_bottom; ++r) {
+        move_cursor(r, 1);
+        printf("\033[K");
     }
-    if (i + 1 < cap) out[i++] = (char)0x9B;
-    if (i < cap) out[i] = '\0';
-    return i;
+    fflush(stdout);
+}
+
+static void log_message(const char* msg, int rx_top, int rx_bottom, int* rx_row)
+{
+    move_cursor(*rx_row, 1);
+    printf("\033[K%s", msg);
+    if (*rx_row >= rx_bottom) {
+        clear_rx_region(rx_top, rx_bottom);
+        *rx_row = rx_top;
+    } else {
+        ++(*rx_row);
+    }
+    fflush(stdout);
+}
+
+static void render_remote_char(unsigned char c,
+                               int* rx_row,
+                               int* rx_col,
+                               int rx_top,
+                               int rx_bottom,
+                               const char* input,
+                               size_t input_len,
+                               int input_row)
+{
+    if (c == 0x9B) {
+        *rx_col = 1;
+        if (*rx_row >= rx_bottom) {
+            clear_rx_region(rx_top, rx_bottom);
+            *rx_row = rx_top;
+        } else {
+            ++(*rx_row);
+        }
+        show_prompt(input, input_len, input_row);
+        return;
+    }
+
+    move_cursor(*rx_row, *rx_col);
+    putchar((int)c);
+    if (*rx_col >= term_cols) {
+        *rx_col = 1;
+        if (*rx_row >= rx_bottom) {
+            clear_rx_region(rx_top, rx_bottom);
+            *rx_row = rx_top;
+        } else {
+            ++(*rx_row);
+        }
+    } else {
+        ++(*rx_col);
+    }
+    fflush(stdout);
+    show_prompt(input, input_len, input_row);
 }
 
 int main(int argc, char** argv)
@@ -81,16 +164,39 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    {
+        struct winsize ws;
+        if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == 0) {
+            if (ws.ws_row > 0) term_rows = ws.ws_row;
+            if (ws.ws_col > 0) term_cols = ws.ws_col;
+        }
+        if (term_rows < 6) term_rows = 6;
+        if (term_rows > 60) term_rows = 24;
+        if (term_cols < 20) term_cols = 40;
+        if (term_cols > 120) term_cols = 80;
+    }
+
+    set_raw_term();
+    printf("\033[2J");
+    move_cursor(1, 1);
     printf("POKEYStream UDP Echo Demo Server\n");
     printf("Listening on UDP port %ld...\n", port);
-    printf("> ");
-    fflush(stdout);
 
     bool have_peer = false;
     struct sockaddr_in peer;
     socklen_t peer_len = sizeof(peer);
-    char rx_line[512];
-    size_t rx_len = 0;
+    char input[256];
+    size_t input_len = 0;
+    int rx_top = 3;
+    int rx_bottom = term_rows - 2;
+    int input_row = term_rows;
+    int rx_row = rx_top;
+    int rx_col = 1;
+
+    clear_rx_region(rx_top, rx_bottom);
+    move_cursor(rx_bottom + 1, 1);
+    for (int i = 0; i < term_cols; ++i) putchar('-');
+    show_prompt(input, input_len, input_row);
 
     for (;;) {
         fd_set rfds;
@@ -112,29 +218,31 @@ int main(int argc, char** argv)
         }
 
         if (FD_ISSET(STDIN_FILENO, &rfds)) {
-            char line[256];
-            if (fgets(line, sizeof(line), stdin)) {
-                if (have_peer) {
-                    char msg[512];
-                    size_t len = 0;
-                    while (line[len] && line[len] != '\n' &&
-                           line[len] != '\r' && len + 1 < sizeof(msg)) {
-                        msg[len] = line[len];
-                        ++len;
+            char ch;
+            ssize_t rn = read(STDIN_FILENO, &ch, 1);
+            if (rn == 1) {
+                if (ch == '\r' || ch == '\n') {
+                    if (have_peer) {
+                        unsigned char eol = 0x9B;
+                        if (sendto(fd, &eol, 1, 0,
+                                   (struct sockaddr*)&peer, peer_len) < 0) {
+                            perror("sendto");
+                        }
                     }
-                    if (len + 1 < sizeof(msg)) {
-                        msg[len++] = (char)0x9B;
-                    }
-                    if (len < sizeof(msg)) msg[len] = '\0';
-                    if (sendto(fd, msg, len, 0,
-                               (struct sockaddr*)&peer, peer_len) < 0) {
-                        perror("sendto");
-                    }
+                    input_len = 0;
+                    show_prompt(input, input_len, input_row);
                 } else {
-                    printf("No client registered yet.\n");
+                    if (input_len + 1 < sizeof(input)) {
+                        input[input_len++] = ch;
+                    }
+                    if (have_peer) {
+                        if (sendto(fd, &ch, 1, 0,
+                                   (struct sockaddr*)&peer, peer_len) < 0) {
+                            perror("sendto");
+                        }
+                    }
+                    show_prompt(input, input_len, input_row);
                 }
-                printf("> ");
-                fflush(stdout);
             }
         }
 
@@ -156,46 +264,33 @@ int main(int argc, char** argv)
                 buf[--msg_len] = '\0';
             }
 
-            char ip[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &from.sin_addr, ip, sizeof(ip));
-
             /* FujiNet Client Registered */
             if (msg_len == 8 && !memcmp(buf, "REGISTER", 8)) {
-                printf("\nClient Registered\n");
+                log_message("Client Registered", rx_top, rx_bottom, &rx_row);
+                rx_col = 1;
                 fflush(stdout);
                 peer = from;
                 peer_len = from_len;
                 have_peer = true;
-                printf("> ");
-                fflush(stdout);
+                rx_col = 1;
+                show_prompt(input, input_len, input_row);
                 /* Break out since we don't need to show the packet info for registration */
                 continue;
             }
-
-            (void)ip;
 
             /* Echo back to whoever spoke last */
             peer = from;
             peer_len = from_len;
             have_peer = true;
-            if (n > 2) {
-                const char* payload = buf + 2;
-                size_t plen = (size_t)(n - 2);
-                bool printed = false;
-                for (size_t i = 0; i < plen; ++i) {
-                    unsigned char c = (unsigned char)payload[i];
-                    if (c == 0x9B) {
-                        rx_line[rx_len] = '\0';
-                        printf("\n%s\n", rx_line);
-                        rx_len = 0;
-                        printed = true;
-                    } else if (rx_len + 1 < sizeof(rx_line)) {
-                        rx_line[rx_len++] = (char)c;
-                    }
+            if (n > 0) {
+                size_t payload_off = 0;
+                if (n >= 3 && (unsigned char)buf[1] == 0x00) {
+                    payload_off = 2;
                 }
-                if (printed) {
-                    printf("> ");
-                    fflush(stdout);
+                for (ssize_t i = (ssize_t)payload_off; i < n; ++i) {
+                    unsigned char c = (unsigned char)buf[i];
+                    render_remote_char(c, &rx_row, &rx_col, rx_top, rx_bottom,
+                                       input, input_len, input_row);
                 }
             }
         }
